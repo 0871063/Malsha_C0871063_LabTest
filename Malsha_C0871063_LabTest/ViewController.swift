c81404c29556156dfc8b917c854938597cb65c82
0871063 <c0871063@mylambton.ca>
2022-11-04 18:26:03 -0400

    Initial Commit

---
+++ Malsha_C0871063_LabTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Malsha_C0871063_LabTest
+//
+//  Created by Malsha Lambton on 2022-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
